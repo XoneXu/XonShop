fb6b79404f2bfdc35f413d9d6895e784a7ada01e
XoneXu <cza.bbb@outlook.com>
2026-05-22 21:58:46 +0200

    Refactor app imports and integrate Vercel analytics and speed insights

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// Krótkie importy dopasowane do Twoich plików
 import { Navbar } from './navbar/navbar';
-import { Home } from './home/home';
 import { Footer } from './footer/footer';
-import { DatePipe } from '@angular/common';
-import { inject as injectVercelAnalytics } from '@vercel/analytics';
-import { injectSpeedInsights } from '@vercel/speed-insights';
 @Component({
   selector: 'app-root',
   standalone: true,
   // Dodajemy krótkie nazwy klas do imports
-  imports: [RouterOutlet, Navbar, Home, Footer, DatePipe],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   title = 'XonShop';
 }
-injectVercelAnalytics();
-injectSpeedInsights();
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-
+import { inject as injectVercelAnalytics } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+.catch((err) => console.error(err));
+injectVercelAnalytics();
+injectSpeedInsights();
