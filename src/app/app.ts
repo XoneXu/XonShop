76e20b9c34148c0ee6b52cc8f40a14e73f8893d7
XoneXu <cza.bbb@outlook.com>
2026-05-22 21:56:29 +0200

    Refactor app configuration and integrate Vercel analytics and speed insights

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 // Krótkie importy dopasowane do Twoich plików
 import { Navbar } from './navbar/navbar';
 import { Home } from './home/home';
 import { Footer } from './footer/footer';
 import { DatePipe } from '@angular/common';
-
+import { inject as injectVercelAnalytics } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -18,3 +18,5 @@ import { DatePipe } from '@angular/common';
 export class App {
   title = 'XonShop';
 }
+injectVercelAnalytics();
+injectSpeedInsights();
